0a95935a7c17c8c9f45a4cb58d21f050540fe065
mohit0103 <90138687+mohit0103@users.noreply.github.com>
2022-03-21 16:19:12 +0530

    Update prac7.py

--- prac7.py
+++ prac7.py
@@ -36,3 +36,90 @@ def sentinel_Search(p, n, key):
         print("Number not found")
 
 sentinel_Search(p, n, key)
+#include<iostream>
+using namespace std;
+ struct node{
+ int value;
+  node* next;
+}*HashTable[10];
+class hashing{
+public:
+
+ hashing(){
+
+  for(int i=0 ; i<10 ; i++){
+   HashTable[i]=NULL;
+  }
+ }
+
+
+ int HashFunction(int value){
+  return (value%10);
+ }
+
+ node* create_node(int x){
+  node* temp=new node;
+  temp->next=NULL;
+  temp->value=x;
+  return temp;
+ }
+
+ void display(){
+  for(int i=0 ; i< 10; i++){
+   node * temp=new node;
+   temp=HashTable[i];
+   cout<<"a["<<i<<"] : ";
+   while(temp !=NULL){
+    cout<<" ->"<<temp->value;
+    temp=temp->next;
+   }
+   cout<<"\n";
+  }
+ }
+ void insertElement(int value){
+   int hash_val = HashFunction(value);
+             // node* prev = NULL;
+              //node* entry = HashTable[hash_val];
+              node* temp=new node;
+              node* head=new node;
+              head = create_node(value);
+              temp=HashTable[hash_val];
+              if (temp == NULL)
+                           {
+
+                              HashTable[hash_val] =head;
+                               }
+              else{
+               while (temp->next != NULL)
+
+              {
+                  temp = temp->next;
+              }
+
+                      temp->next =head;
+
+              }
+
+
+ }
+};
+
+int main(){
+ int ch;
+ int data,search,del;
+ hashing h;
+ do{
+  cout<<"\nTelephone : \n1.Insert \n2.Display";
+  cin>>ch;
+  switch(ch){
+  case 1:cout<<"\nEnter phone no. to be inserted : ";
+    cin>>data;
+    h.insertElement(data);
+    break;
+  case 2:h.display();
+    break;
+  }
+ }while(ch!=5);
+ return 0;
+}
+
